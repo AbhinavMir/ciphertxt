e7bc5c8d76d7797d9e94f10e4ec17870833e03b3
Abhinav Srivastava <atg271@gmail.com>
2023-07-24 19:06:30 +0530

    wip

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-// next.config.js
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.jsx');
-
-module.exports = withNextra({
-  // Your other Next.js configuration options go here
-
-  // Define the default meta tags for all pages
-  head: () => ({
-    title: 'Your Site Title',
-    meta: [
-      {
-        name: 'description',
-        content: 'Your default site description goes here',
-      },
-      // Add other meta tags here if needed
-    ],
-  }),
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  latex: true // Add the 'latex' option here,
 });
+
+module.exports = withNextra();
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router' 
+import { useNextSeoProps } from 'nextra-theme-docs'
+
 export default {
     logo: <span>Ciphertxt.com</span>,
     project: {
       link: 'https://github.com/abhinavmir/ciphertxt',
+    },
+    useNextSeoProps() {
+      return {
+        title: 'CipherTxt ジ', 
+        description: 'A corpus of exhaustive notes on Cryptography, Secure communication, Network security, Formal Verification, and Structure of Programming Languages.'
+      }
     }
-    // ...
   }
